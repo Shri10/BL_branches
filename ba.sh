b3808e57214b50dac3558b2b366c2d85b5f490f7
Shri10 <pethe.shriraj@gmail.com>
2023-04-18 17:02:16 +0530

    UC3

--- ba.sh
+++ ba.sh
@@ -1,15 +1,15 @@
-EMP_PER_HOUR_RATE=20
-WORKING_HOURS=8
+isFullTime=1
+isPartTime=2
+empHourRate=20
+randomCheck=$((RANDOM%3))
 
-empCheck=$((RANDOM%2))
-totalSalary=0
-
-if [ $empCheck -eq 1 ]
-then
-        echo "Employee is Present"
-        totalSalary=$(( EMP_PER_HOUR_RATE * WORKING_HOURS ))
+if [ $isFullTime -eq $randomCheck ]; then
+    empHrs=8
+elif [ $isPartTime -eq $randomCheck ]; then
+    empHrs=4
 else
-        echo "Employee is Absent"
+    empHrs=0
 fi
 
-echo "Total Salary: $totalSalary"
+salary=$(($empHrs*$empHourRate))
+echo "Salary: " $salary
