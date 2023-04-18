106db8df9c3ea49b13a3b02a9a72048a1f0869a3
Shri10 <pethe.shriraj@gmail.com>
2023-04-18 15:04:34 +0530

    UC2 Modified

--- ba.sh
+++ ba.sh
@@ -1,15 +1,5 @@
-EMP_PER_HOUR_RATE=20
-WORKING_HOURS=8
+WagePerHour=20
+FullDayHour=8
 
-empCheck=$((RANDOM%2))
-totalSalary=0
-
-if [ $empCheck -eq 1 ]
-then
-        echo "Employee is Present"
-        totalSalary=$(( EMP_PER_HOUR_RATE * WORKING_HOURS ))
-else
-        echo "Employee is Absent"
-fi
-
-echo "Total Salary: $totalSalary"
+        totalSalary=$(( WagePerHour * FullDayHour ))
+echo "Daily Employee Wage: $totalSalary"
