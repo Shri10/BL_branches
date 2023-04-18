3ceda45b0b80fad1a07122f5e955f714336426ec
Shri10 <pethe.shriraj@gmail.com>
2023-04-18 17:07:03 +0530

    UC4

--- ba.sh
+++ ba.sh
@@ -3,13 +3,17 @@ isPartTime=2
 empHourRate=20
 randomCheck=$((RANDOM%3))
 
-if [ $isFullTime -eq $randomCheck ]; then
-    empHrs=8
-elif [ $isPartTime -eq $randomCheck ]; then
-    empHrs=4
-else
-    empHrs=0
-fi
+case $randomCheck in
+    $isFullTime)
+        empHrs=8
+        ;;
+    $isPartTime)
+        empHrs=4
+        ;;
+    *)
+        empHrs=0
+        ;;
+esac
 
 salary=$(($empHrs*$empHourRate))
 echo "Salary: " $salary
