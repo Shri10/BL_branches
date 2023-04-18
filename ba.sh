d5c16654fb75c3a798b32608a796de21530a0d22
Shri10 <pethe.shriraj@gmail.com>
2023-04-18 15:00:42 +0530

    This is UC2

--- ba.sh
+++ ba.sh
@@ -1,5 +1,15 @@
-if (( RANDOM % 2 == 1 )); then
-    echo "Employee is Present"
+EMP_PER_HOUR_RATE=20
+WORKING_HOURS=8
+
+empCheck=$((RANDOM%2))
+totalSalary=0
+
+if [ $empCheck -eq 1 ]
+then
+        echo "Employee is Present"
+        totalSalary=$(( EMP_PER_HOUR_RATE * WORKING_HOURS ))
 else
-    echo "Employee is Absent"
+        echo "Employee is Absent"
 fi
+
+echo "Total Salary: $totalSalary"
